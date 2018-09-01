cd7d0f2a866f7014a3140128492adef452e6ddef
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-09-01 17:28:48 +0300

    WIP Cleanup before adding Jest

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
 import Plasmoid from '../build/wrappers/Plasmoid'
 import MintableToken from '../build/wrappers/MintableToken'
-import * as ethUtil from 'ethereumjs-util'
 
 export {
   Plasmoid,
   MintableToken
-}
-
-export function randomId (digits: number = 3) {
-  const datePart = new Date().getTime() * Math.pow(10, digits)
-  const extraPart = Math.floor(Math.random() * Math.pow(10, digits)) // 3 random digits
-  return datePart + extraPart // 16 digits
-}
-
-export function channelId (sender: string, receiver: string): string {
-  let random = randomId()
-  let buffer = ethUtil.sha3(sender + receiver + random)
-  return ethUtil.bufferToHex(buffer)
 }
