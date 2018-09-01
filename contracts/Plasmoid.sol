c27ef32972703452b206c92ec82d1fbad8b2fdcf
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-09-01 19:40:05 +0300

    Basic channel structure

--- contracts/Plasmoid.sol
+++ contracts/Plasmoid.sol
@@ -7,25 +7,45 @@ import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 contract Plasmoid {
     using SafeMath for uint256;
 
-    mapping (address => uint256) private balances;
+    mapping (uint256 => uint256) private balances;
+    mapping (uint256 => address) public owners;
+
+    uint256 public uid;
+
     StandardToken public token;
+
+    event DidDeposit(address indexed owner, uint256 indexed uid, uint256 amount);
+    event DidWithdraw(address indexed owner, uint256 indexed uid, uint256 amount);
 
     constructor (address _tokenAddress) public {
         token = StandardToken(_tokenAddress);
+        uid = 0;
     }
 
-    function balanceOf (address owner) public view returns (uint256) {
-        return balances[owner];
+    function balanceOf (uint256 _uid) public view returns (uint256) {
+        return balances[_uid];
     }
 
     function deposit (uint256 amount) public {
-        token.transferFrom(msg.sender, address(this), amount);
-        balances[msg.sender] = balances[msg.sender].add(amount);
+        require(amount > 0, "Can not deposit 0");
+        require(token.transferFrom(msg.sender, address(this), amount));
+        uid = uid.add(1);
+        balances[uid] = amount;
+        owners[uid] = msg.sender;
+
+        emit DidDeposit(msg.sender, uid, amount);
     }
 
-    function withdraw () public {
-        require(balances[msg.sender] > 0, "Balance is 0");
-        require(token.transfer(msg.sender, balances[msg.sender]), "Can not transfer tokens");
-        balances[msg.sender] = 0;
+    function withdraw (uint256 _uid) public {
+        address owner = owners[_uid];
+        uint256 amount = balances[_uid];
+
+        require(amount > 0, "Balance is 0");
+        require(owner == msg.sender, "Only owner can withdraw");
+        require(token.transfer(owner, amount), "Can not transfer tokens");
+        delete balances[_uid];
+        delete owners[_uid];
+
+        emit DidWithdraw(owner, uid, amount);
     }
 }
--- src/Plasmoid.test.ts
+++ src/Plasmoid.test.ts
@@ -1,4 +1,5 @@
 import * as contracts from './index'
+import { BigNumber } from 'bignumber.js'
 
 const Plasmoid = artifacts.require<contracts.Plasmoid.Contract>('Plasmoid.sol')
 const MintableToken = artifacts.require<contracts.MintableToken.Contract>('MintableToken.sol')
@@ -21,44 +22,78 @@ contract('Plasmoid', accounts => {
     await mintableToken.finishMinting({from: tokenOwner})
   })
 
-  test('deposit', async () => {
-    const participantBefore = await mintableToken.balanceOf(participant)
-    const accountBefore = await plasmoid.balanceOf(participant)
-    const plasmoidBalanceBefore = await mintableToken.balanceOf(plasmoid.address)
-    expect(accountBefore.toNumber()).toEqual(0)
-    expect(plasmoidBalanceBefore.toNumber()).toEqual(0)
+  describe('deposit', () => {
+    test('move token to contract', async () => {
+      const participantBefore = await mintableToken.balanceOf(participant)
+      const plasmoidBalanceBefore = await mintableToken.balanceOf(plasmoid.address)
+      expect(plasmoidBalanceBefore.toNumber()).toEqual(0)
 
-    await mintableToken.approve(plasmoid.address, VALUE, {from: participant})
-    await plasmoid.deposit(VALUE, { from: participant })
+      await mintableToken.approve(plasmoid.address, VALUE, {from: participant})
+      await plasmoid.deposit(VALUE, { from: participant })
 
-    const participantAfter = await mintableToken.balanceOf(participant)
-    const accountAfter = await plasmoid.balanceOf(participant)
-    const plasmoidBalanceAfter = await mintableToken.balanceOf(plasmoid.address)
+      const participantAfter = await mintableToken.balanceOf(participant)
+      const plasmoidBalanceAfter = await mintableToken.balanceOf(plasmoid.address)
 
-    expect(participantAfter.toNumber()).toEqual(participantBefore.toNumber() - VALUE)
-    expect(accountAfter.toNumber()).toEqual(accountBefore.toNumber() + VALUE)
-    expect(plasmoidBalanceAfter.toNumber()).toEqual(VALUE)
+      expect(participantAfter.toNumber()).toEqual(participantBefore.toNumber() - VALUE)
+      expect(plasmoidBalanceAfter.toNumber()).toEqual(VALUE)
+    })
+    test('emit event', async () => {
+      await mintableToken.approve(plasmoid.address, VALUE, {from: participant})
+      const tx = await plasmoid.deposit(VALUE, { from: participant })
+      const event = tx.logs[0]
+      expect(event.event).toEqual('DidDeposit')
+      expect(event.args.owner).toEqual(participant)
+      expect(event.args.amount.toNumber()).toEqual(VALUE)
+    })
+    test('set balance', async () => {
+      await mintableToken.approve(plasmoid.address, VALUE, {from: participant})
+      const tx = await plasmoid.deposit(VALUE, { from: participant })
+      const uid = tx.logs[0].args.uid as BigNumber
+
+      const accountAfter = await plasmoid.balanceOf(uid)
+      expect(accountAfter.toNumber()).toEqual(VALUE)
+    })
   })
 
-  test('withdraw', async () => {
-    await mintableToken.approve(plasmoid.address, VALUE, {from: participant})
-    await plasmoid.deposit(VALUE, { from: participant })
+  describe('withdraw', () => {
+    let uid: BigNumber
 
-    const participantBefore = await mintableToken.balanceOf(participant)
-    const accountBefore = await plasmoid.balanceOf(participant)
-    const plasmoidBalanceBefore = await mintableToken.balanceOf(plasmoid.address)
-    expect(participantBefore.toNumber()).toEqual(MINTED - VALUE)
-    expect(accountBefore.toNumber()).toEqual(VALUE)
-    expect(plasmoidBalanceBefore.toNumber()).toEqual(VALUE)
+    beforeEach(async () => {
+      await mintableToken.approve(plasmoid.address, VALUE, {from: participant})
+      const tx = await plasmoid.deposit(VALUE, { from: participant })
+      uid = tx.logs[0].args.uid as BigNumber
+    })
 
-    await plasmoid.withdraw({from: participant})
+    test('withdraw token from contract', async () => {
+      const participantBefore = await mintableToken.balanceOf(participant)
+      const plasmoidBalanceBefore = await mintableToken.balanceOf(plasmoid.address)
+      expect(participantBefore.toNumber()).toEqual(MINTED - VALUE)
+      expect(plasmoidBalanceBefore.toNumber()).toEqual(VALUE)
 
-    const participantAfter = await mintableToken.balanceOf(participant)
-    const accountAfter = await plasmoid.balanceOf(participant)
-    const plasmoidBalanceAfter = await mintableToken.balanceOf(plasmoid.address)
+      await plasmoid.withdraw(uid,{from: participant})
 
-    expect(participantAfter.toNumber()).toEqual(MINTED)
-    expect(accountAfter.toNumber()).toEqual(0)
-    expect(plasmoidBalanceAfter.toNumber()).toEqual(0)
+      const participantAfter = await mintableToken.balanceOf(participant)
+      const plasmoidBalanceAfter = await mintableToken.balanceOf(plasmoid.address)
+
+      expect(participantAfter.toNumber()).toEqual(MINTED)
+      expect(plasmoidBalanceAfter.toNumber()).toEqual(0)
+    })
+    test('emit event', async () => {
+      let tx = await plasmoid.withdraw(uid,{from: participant})
+      let event = tx.logs[0]
+      expect(event.event).toEqual('DidWithdraw')
+      expect(event.args.uid).toEqual(uid)
+      expect(event.args.owner).toEqual(participant)
+      expect(event.args.amount.toNumber()).toEqual(VALUE)
+    })
+    test('set balance', async () => {
+      const balanceBefore = await plasmoid.balanceOf(uid)
+      expect(balanceBefore.toNumber()).toEqual(VALUE)
+
+      await plasmoid.withdraw(uid,{from: participant})
+
+      const balanceAfter = await plasmoid.balanceOf(uid)
+      expect(balanceAfter.toNumber()).toEqual(0)
+    })
   })
 })
