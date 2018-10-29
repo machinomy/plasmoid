1acef315bfa2cda653afe60aa12f815cd871bec3
Andrei Riaskov <ryaskov.work@gmail.com>
2018-10-29 16:35:38 +0400

    Refactor migration for TestToken in Truffle 5 style.

--- migrations/5_deploy_test_token.ts
+++ migrations/5_deploy_test_token.ts
@@ -3,10 +3,8 @@ import * as Deployer from 'truffle-deployer'
 const ECRecovery = artifacts.require('./ECRecovery.sol')
 const TestToken = artifacts.require('./TestToken.sol')
 
-module.exports = function (deployer: Deployer) {
-  return deployer.deploy(ECRecovery).then(() => {
-    return deployer.link(ECRecovery, TestToken).then(() => {
-      return deployer.deploy(TestToken)
-    })
-  })
+module.exports = async function (deployer: Deployer) {
+  await deployer.deploy(ECRecovery)
+  await deployer.link(ECRecovery, TestToken)
+  return deployer.deploy(TestToken)
 }
